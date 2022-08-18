a2971a2eb2902777d91f8ac7c458a7862abb7a9e
Prabir Singh <codeprabir@gmail.com>
2022-08-18 13:32:26 +0530

    product page updated from database

--- components/ProductItem.js
+++ components/ProductItem.js
@@ -1,16 +1,16 @@
 /* eslint-disable @next/next/no-img-element */
-import React from 'react'
 import Link from 'next/link'
+import React from 'react'
 
-export default function ProductItem({ product }){
+export default function ProductItem({ product, addToCartHandler }) {
   return (
     <div className='card'>
       <Link href={`/product/${product.slug}`}>
         <a>
           <img
             src={product.image}
-            alt={product.image}
-            className='rounded shadow'
+            alt={product.name}
+            className='rounded shadow object-cover h-64 w-full'
           />
         </a>
       </Link>
@@ -21,8 +21,12 @@ export default function ProductItem({ product }){
           </a>
         </Link>
         <p className='mb-2'>{product.brand}</p>
-        <p className='mb-2'>${product.price}</p>
-        <button className='primary-button' type='button'>
+        <p>${product.price}</p>
+        <button
+          className='primary-button'
+          type='button'
+          onClick={() => addToCartHandler(product)}
+        >
           Add to cart
         </button>
       </div>
--- utils/Store.js
+++ utils/Store.js
@@ -51,6 +51,14 @@ function reducer(state, action) {
           },
         },
       }
+    case 'SAVE_PAYMENT_METHOD':
+      return {
+        ...state,
+        cart: {
+          ...state.cart,
+          PaymentMethod: action.payload,
+        },
+      }
     default:
       return state
   }
